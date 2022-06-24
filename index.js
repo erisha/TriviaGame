52116536243409b5aa452a99845d07218a2193e6
erisha <forever.erisha@gmail.com>
2022-06-23 22:17:54 -0400

    75% done. working on JS. adding questions next

---
+++ index.js
@@ -0,0 +1,18 @@
+const userScore = document.getElementById('userScore');
+const trueBtn = document.getElementById('true');
+const falseBtn = document.getElementById('false');
+const choiceA = document.getElementById('choiceA');
+const choiceB = document.getElementById('choiceB');
+const choiceC = document.getElementById('choiceC');
+const choiceD = document.getElementById('choiceD');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+const restartBtn = document.getElementById('restart');
+const question = document.getElementById('question')
+
+
+
+
+
+
+
